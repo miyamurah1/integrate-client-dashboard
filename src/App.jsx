fbea9f995a4dd8c35d2c3490c70c310eb0321ecb
miyamurah1 <miyamurah57@gmail.com>
2026-03-03 09:34:25 +0530

    Day 2 progress: added sidebar icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Sidebar from "./components/Sidebar";
+import ChartSection from "./components/ChartSection";
 import Topbar from "./components/Topbar";
 import StatCard from "./components/StatCard";
 import InfoCard from "./components/InfoCard";
@@ -13,12 +14,13 @@ function App() {
         <Topbar />
 
         <div className="stats">
-          <StatCard title="Total Revenue" value="$24,500" />
-          <StatCard title="Active Users" value="1,245" />
-          <StatCard title="New Signups" value="342" />
+          <StatCard title="Total Revenue" value="$24,500" growth="+12%" />
+          <StatCard title="Active Users" value="1,245" growth="+8%" />
+          <StatCard title="New Signups" value="342" growth="+5%" />
         </div>
 
         <div className="grid">
+          <ChartSection />
           <InfoCard title="Analytics Overview" />
           <InfoCard title="Room Occupancy" />
           <InfoCard title="Bookings Summary" />

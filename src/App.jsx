1cc876f727822789b8c02c3a9045ec3a4e6098f4
miyamurah1 <miyamurah57@gmail.com>
2026-03-03 09:49:00 +0530

    Integrated revenue chart into dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Sidebar from "./components/Sidebar";
-import ChartSection from "./components/ChartSection";
 import Topbar from "./components/Topbar";
 import StatCard from "./components/StatCard";
 import InfoCard from "./components/InfoCard";
+import ChartSection from "./components/ChartSection";
 import "./App.css";
 
 function App() {
@@ -13,15 +13,16 @@ function App() {
       <div className="main">
         <Topbar />
 
+        {/* Stats Section */}
         <div className="stats">
           <StatCard title="Total Revenue" value="$24,500" growth="+12%" />
           <StatCard title="Active Users" value="1,245" growth="+8%" />
           <StatCard title="New Signups" value="342" growth="+5%" />
         </div>
 
+        {/* Grid Section */}
         <div className="grid">
           <ChartSection />
-          <InfoCard title="Analytics Overview" />
           <InfoCard title="Room Occupancy" />
           <InfoCard title="Bookings Summary" />
           <InfoCard title="Calendar Section" />
